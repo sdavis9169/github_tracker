f3cd4c2f82549bdbb596133b3ae0731bd70377fe
Sdavis <stevendavis0622@gmail.com>
2020-07-07 14:56:19 -0600

    react router, about

--- src/App.js
+++ src/App.js
@@ -1,33 +1,82 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/Layout/Navbar';
+import Search from './components/users/Search';
 import './App.css';
 import axios from 'axios';
 import Users from './components/users/Users';
+import Alert from './components/Layout/Alert';
+import About from './components/Pages/About';
 
 export default class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
-  async componentDidMount() {
+  //Seach Users
+  searchUsers = async text => {
     this.setState({ loading: true });
-
     const res = await axios.get(
-      `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
     );
 
-    this.setState({ users: res.data, loading: false });
-    console.log(res.data);
-  }
+    this.setState({ users: res.data.items, loading: false });
+    console.log(res.data.items);
+  };
+
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
   render() {
+    const { users, loading } = this.state;
+
     return (
-      <div>
-        <Navbar />
+      <Router>
         <div>
-          <Users loading={this.state.loading} users={this.state.users} />
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/About' component={About} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
+
+// async componentDidMount() {
+//   this.setState({ loading: true });
+
+//   const res = await axios.get(
+//     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+//       &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+//   );
+
+//   this.setState({ users: res.data, loading: false });
+//   console.log(res.data);
+// }
