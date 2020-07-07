5321c1ce2a4409137bff902173b78a0f8ac72489
Sdavis <stevendavis0622@gmail.com>
2020-07-07 11:57:03 -0600

    displaying users

--- src/components/Layout/Navbar.jsx
+++ src/components/Layout/Navbar.jsx
@@ -1,16 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-export class Navbar extends Component {
-  render() {
-    return (
-      <nav className='navbar  bg-primary'>
-        <h1>
-          <i className='fab fa-github' />
-          Navbar
-        </h1>
-      </nav>
-    );
-  }
-}
+const Navbar = props => {
+  return (
+    <nav className='navbar  bg-primary'>
+      <h1>
+        <i className={props.img} /> {props.title}
+      </h1>
+    </nav>
+  );
+};
+
+Navbar.defaultProps = {
+  title: 'Github Finder',
+  icon: 'fab fa-github'
+};
+
+Navbar.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired
+};
 
 export default Navbar;
